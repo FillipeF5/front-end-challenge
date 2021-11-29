911240c052a0400ed150d19ff558a2d8a3ef3f23
FillipeF5 <fillipefreitas220@gmail.com>
2021-11-29 16:40:12 -0300

    js counter done

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ let contador = document.getElementById('contador');
 let start = 0;
 contador.innerHTML = start;
 
-mais.addEventListener('click', function() {
+mais.addEventListener('click', function(event) {
     contador.innerHTML = ++start;
+    event.preventDefault();
 });
 
-menos.addEventListener('click', function() {
+menos.addEventListener('click', function(event) {
     contador.innerHTML = --start;
+    event.preventDefault()
 });
